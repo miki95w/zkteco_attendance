49b8ba61943b65a0a134d82ff859fbf9f07fa2c8
Miki <mikiasworku87@gmail.com>
2026-07-24 09:23:47 +0300

    absence

--- models/absence_report.py
+++ models/absence_report.py
@@ -11,33 +11,16 @@ class AttendanceAbsenceReport(models.Model):
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, 'attendance_absence_report')
-        self.env.cr.execute(f"""
+        self.env.cr.execute("""
             CREATE OR REPLACE VIEW attendance_absence_report AS (
-                WITH RECURSIVE date_range AS (
-                    SELECT MIN(date_start) as d_date, MAX(date_end) as max_date FROM zkteco_employee_shift
-                    UNION ALL
-                    SELECT d_date + 1, max_date FROM date_range WHERE d_date < max_date
-                ),
-                expected_work AS (
-                    SELECT
-                        es.employee_id,
-                        dr.d_date as work_date,
-                        es.shift_id
-                    FROM
-                        date_range dr
-                    JOIN
-                        zkteco_employee_shift es ON dr.d_date BETWEEN es.date_start AND es.date_end
-                )
                 SELECT
-                    row_number() over (order by ew.employee_id, ew.work_date) as id,
-                    ew.employee_id as employee_id,
-                    ew.work_date as date,
-                    ew.shift_id as shift_id
+                    id,
+                    employee_id,
+                    date,
+                    shift_id
                 FROM
-                    expected_work ew
-                LEFT JOIN
-                    hr_attendance ha ON ha.employee_id = ew.employee_id AND CAST(ha.check_in AS DATE) = ew.work_date
+                    zkteco_attendance_record
                 WHERE
-                    ha.id IS NULL
+                    status = 'absent'
             )
         """)
