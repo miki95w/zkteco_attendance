d3b000d1963d6fdc34dcdc3608a810300cac6d54
Miki <mikiasworku87@gmail.com>
2026-07-24 09:27:51 +0300

    hr

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -4,4 +4,5 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     zkteco_device_id = fields.Char(string='ZKTeco Device User ID', help="The User ID used on the ZKTeco device.")
+    work_policy_id = fields.Many2one('zkteco.work.policy', string='Work Policy')
     attendance_record_ids = fields.One2many('zkteco.attendance.record', 'employee_id', string='Attendance History')
